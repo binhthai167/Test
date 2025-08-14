d630c0c18dd658329faaed7f5a3df1551bfa27ac
Le Thai Binh <250232@NSVC.nidec-sankyo.co.jp>
2025-08-14 14:39:55 +0700

    lan cuoi

--- addtodict.py
+++ addtodict.py
@@ -9,16 +9,3 @@
 #     for word in sorted(unique_words):
 #         f.write(word + "\n")
 
-from spellchecker import SpellChecker
-
-spell = SpellChecker()
-
-# find those words that may be misspelled
-misspelled = spell.unknown(['something', 'is', 'hapenning', 'here'])
-
-for word in misspelled:
-    # Get the one `most likely` answer
-    print(spell.correction(word))
-
-    # Get a list of `likely` options
-    print(spell.candidates(word))
